354ba2078c18887f528bd4a0e45553b1d263c2b6
NinjonikSVK <petricekzatko@gmail.com>
2023-11-28 22:10:43 +0100

    first commit

---
+++ src/app/components/MessageLink.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+interface MessageLinkProps {}
+
+const MessageLink: FC<MessageLinkProps> = () => (
+  <div>
+    MessageLink Component
+  </div>
+);
+
+export default MessageLink;
---
+++ src/app/components/MessagesSection.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+interface MessagesSectionProps {}
+
+const MessagesSection: FC<MessagesSectionProps> = () => (
+  <div>
+    MessagesSection Component
+  </div>
+);
+
+export default MessagesSection;

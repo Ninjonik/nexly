7a911cc8d1e0a7b204c0480ab958924e4d1cde74
NinjonikSVK <petricekzatko@gmail.com>
2023-11-28 22:11:34 +0100

    first

--- src/app/components/MessageLink.tsx
+++ src/app/components/MessageLink.tsx
@@ -1,12 +1,38 @@
+"use client"
+
 import React, { FC } from 'react';
 
 
-interface MessageLinkProps {}
+interface MessageLinkProps {
+    typing: boolean,
+    time: number,
+    notifications: number,
+}
 
-const MessageLink: FC<MessageLinkProps> = () => (
-  <div>
-    MessageLink Component
-  </div>
+const MessageLink: FC<MessageLinkProps> = ({ typing, time, notifications }) => (
+    <div className="flex flex-row justify-between items-center gap-4 group">
+        <div className="w-[5dvw] h-[7dvh]">
+            <img className="w-full h-full rounded-full" src="https://openseauserdata.com/files/3085b3fc65f00b28699b43efb4434eec.png" alt='profile picture'/>
+            <div className="group-hover:border-blue relative bottom-6 left-12 border-2 border-light bg-green-500 h-6 w-6 text-lg rounded-full text-white"></div>
+        </div>
+
+        <div className="flex flex-row justify-between w-full">
+            <div className="flex flex-col justify-between w-7/10">
+                <h3 className="text-lg font-bold">Leila</h3>
+                {typing ?
+                    <span className="italic text-sm text-blue">Typing...</span>
+                    :
+                    <span className="text-sm text-lightly">OK! See ya tomorrow!</span>
+                }
+
+            </div>
+
+            <div className="flex flex-col justify-between items-end w-3/10">
+                {time ? <h3 className="text-md text-lightly font-bold">8:30 PM</h3> : null}
+                {notifications ? <span className="flex justify-center text-center items-center bg-blue h-6 w-6 text-lg rounded-full text-white">{ notifications }</span> : null}
+            </div>
+        </div>
+    </div>
 );
 
 export default MessageLink;
--- src/app/components/MessagesSection.tsx
+++ src/app/components/MessagesSection.tsx
@@ -1,12 +1,24 @@
-import React, { FC } from 'react';
+import React, {FC, ReactNode} from 'react';
+import MessageLink from "@/app/components/MessageLink";
 
 
-interface MessagesSectionProps {}
+interface MessagesSectionProps {
+    title: string,
+    icon: ReactNode,
+}
 
-const MessagesSection: FC<MessagesSectionProps> = () => (
-  <div>
-    MessagesSection Component
-  </div>
+const MessagesSection: FC<MessagesSectionProps> = ({ title, icon }) => (
+    <div className="flex flex-col gap-2 w-full">
+
+        <div className="text-lightly">{icon} { title }</div>
+
+        <MessageLink  notifications={1} time={Date.now()} typing={false}/>
+
+        <hr className="w-full text-heavily my-2" />
+
+        <MessageLink  notifications={0} time={Date.now()} typing={true}/>
+
+    </div>
 );
 
 export default MessagesSection;

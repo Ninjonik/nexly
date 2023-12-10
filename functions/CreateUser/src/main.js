3fd5c5de9a5e851bb07b21d81b9d68d2d31dbfe9
NinjonikSVK <petricekzatko@gmail.com>
2023-12-10 16:52:48 +0100

    - createUser function fixed

--- functions/CreateUser/src/main.js
+++ functions/CreateUser/src/main.js
@@ -1,4 +1,4 @@
-import { Client, Databases } from 'node-appwrite';
+import { Client, Databases, ID } from 'node-appwrite';
 
 export default async ({ req, res, log, error }) => {
   const client = new Client()
@@ -10,39 +10,32 @@ export default async ({ req, res, log, error }) => {
 
   if (req.method === 'POST') {
     try {
-      // Parse the request body to get the new user details
       const newUser = req.body;
 
-      console.log(newUser);
+      log(newUser);
 
-      // Get the user ID and username from the new user data
-      const authId = newUser.$id; // Assuming $id contains the user ID
-      const username = newUser.username;
+      const authId = newUser.$id;
+      const email = newUser.name;
 
-      // Create a new record in the 'users' collection in the 'nexly' database
       const userRecord = await database.createDocument(
           process.env.APPWRITE_FUNCTION_DATABASE_ID,
           'users',
           ID.unique(),
           {
             authId,
-            username,
+            email,
           }
       );
 
-      // Log the newly created user record
       log('New user record created:', userRecord);
 
-      // Send a response indicating success
       return res.json({
         success: true,
         message: 'User record created successfully',
       });
     } catch (err) {
-      // Log any errors that occur during the process
       error('Error creating user record:', err);
 
-      // Send a response indicating failure
       return res.json({
         success: false,
         message: 'Error creating user record',
@@ -50,11 +43,8 @@ export default async ({ req, res, log, error }) => {
     }
   }
 
-  // If the request method is not 'POST', send a default response
   return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
+    success: false,
+    message: 'Invalid method',
   });
 };

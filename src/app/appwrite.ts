5b1374cae9c29b5aca63ef5e69b1b34b92d950e7
NinjonikSVK <petricekzatko@gmail.com>
2023-12-10 16:21:21 +0100

    - added test cloud function

--- src/app/appwrite.ts
+++ src/app/appwrite.ts
@@ -1,16 +1,36 @@
-import {Account, Client} from 'appwrite';
+import {Account, Client, Functions} from 'appwrite';
 
 const appwriteEndpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
 const appwriteProject = process.env.NEXT_PUBLIC_APPWRITE_PROJECT;
 const appwriteKey = process.env.APPWRITE_KEY;
 
 export const client = new Client();
+const functions = new Functions(client)
 
 if (appwriteEndpoint && appwriteProject) {
     client
         .setEndpoint(appwriteEndpoint)
         .setProject(appwriteProject);
     // .setKey(appwriteKey);
+
+    const funcTest = async () => {
+        try {
+            const execution = await functions.createExecution(
+                '6575d1384f7e51cf0146',
+                JSON.stringify({ 'foo': 'bar' }),
+                false,
+                '/',
+                'GET',
+                { 'X-Custom-Header': '123' }
+            )
+            console.log(execution)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    funcTest()
+
 } else {
     console.error("Please make sure APPWRITE_ENDPOINT and APPWRITE_PROJECT are defined in your environment variables.");
 }
